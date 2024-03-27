609dfbbac6017184e3bb5ba0bd614b5bfdd01dcb
Wojciech Tokarz <wtokarz@student.agh.edu.pl>
2024-03-27 15:21:58 +0100

    checkpoint

---
+++ src/algorithms/genetic/selection.py
@@ -0,0 +1,5 @@
+from operators import calculate_times
+from src.algorithms.interface import AlgorithmOutput
+from typing import List
+
+def rank_selection(population: List[AlgorithmOutput], costs: List[int]) -> List[AlgorithmOutput]:
