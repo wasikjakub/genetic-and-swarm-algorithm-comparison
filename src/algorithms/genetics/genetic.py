ed010d42cffdd5f764525cc947841a251e034c4b
Jakub Wasik <144064944+wasikjakub@users.noreply.github.com>
2024-04-14 20:48:27 +0200

    Synchronize updates

--- src/algorithms/genetics/genetic.py
+++ src/algorithms/genetics/genetic.py
@@ -1,13 +1,13 @@
 import random
 from math import ceil
-from algorithms.genetics.operators import crossover, shuffle_mutate, mutate_by_add_random_node, value_change_mutation, swap_mutate
+from algorithms.genetics.operators import crossover, shuffle_mutate, mutate_by_add_random_node, value_change_mutation, swap_mutate, two_point_crossover
 from algorithms.genetics.other import generate_population, calculate_all, rank_selection,\
     calculate_one, parents_to_population_rate, get_weights, tournament_selection, roulette_selection, modified_proportional_selection, truncation_selection
 from algorithms.genetics.transform_graph import change_graph
 import networkx as nx
 
 class GeneticAlgorithm:
-    def __init__(self, order, warehouse, selection_method, mutation_method, population=None) -> None:
+    def __init__(self, order, warehouse, selection_method, mutation_method, crossover_method, population=None) -> None:
         self.order = order
         self.warehouse = warehouse
         self.population = population  # [(AlgorithmOutput, cost)]
@@ -16,6 +16,7 @@ class GeneticAlgorithm:
         self.best_list = []
         self.selection = selection_method
         self.mutation = mutation_method
+        self.crossover = crossover_method
 
     def run(self, max_iter, population_count, mutation_rate=0.2):
         self.warehouse.graph = change_graph(self.warehouse.graph, self.order)
@@ -49,13 +50,14 @@ class GeneticAlgorithm:
         elif self.selection == 'truanction':
             to_crossover = truncation_selection(self.population)
         for _ in range(len(self.population)-len(temp_population)):
-            # if len(to_crossover) < 2:
-            #     break
             parents = random.sample(to_crossover, 2)
             to_crossover.remove(parents[0])
             to_crossover.remove(parents[1])
             # crossover
-            child = crossover(parents[0], parents[1], self.order, self.weights, self.warehouse)
+            if self.crossover == 'crossover':
+                child = crossover(parents[0], parents[1], self.order, self.weights, self.warehouse)
+            elif self.crossover == 'two_point_crossover':
+                child = two_point_crossover(parents[0], parents[1], self.order, self.weights, self.warehouse)
             nodes = nx.number_of_nodes(self.warehouse.graph)
             # mutation
             if self.mutation == 'shuffle':
@@ -73,42 +75,3 @@ class GeneticAlgorithm:
             temp_population.append((child, cost))
             temp_population.sort(key=lambda tup: tup[1])
             self.population = temp_population
-
-# import networkx as nx
-#
-#
-# START_NODE = '0'
-# graph = nx.read_adjlist('../../../generated_graphs/graph_4_4.adjlist')
-# G = nx.path_graph(graph)  # or DiGraph, MultiGraph, MultiDiGraph, etc
-# points = [{n: list(nbrdict.keys())} for n, nbrdict in G.adjacency()]
-#
-# shortest_paths = nx.shortest_path(graph)
-#
-# orders = {
-#     7: 1,
-#     11: 1,
-#     9: 3,
-#     8: 1,
-#     6: 12,
-#     1: 4,
-#     2: 2,
-# }
-# nodes = list(orders.keys())
-# nodes = [START_NODE] + [str(node) for node in nodes]
-# complete_graph = nx.Graph()
-#
-# for src in nodes:
-#     for dst in nodes:
-#         if src == dst:
-#             continue
-#         path = shortest_paths[src][dst]
-#         complete_graph.add_edge(src, dst, weight=len(path)-1, path=path, sum_load=0, feromone=0)
-#
-# print()
-
-
-# nx.set_node_attributes(complete_graph, {
-#     node: {"weight": orders.get(int(node), np.inf)}
-#     for node in nodes
-# })
-# list(complete_graph.nodes())
